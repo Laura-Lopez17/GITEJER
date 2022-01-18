9db1c341004f2f2063c6a087d9ec930c0505e378
Laura-Lopez17 <al038381@ieslaencanta.com>
2022-01-18 19:14:45 +0100

    commit 2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
-        // put your code here
+        echo "AÑADIDAS LAS ESTRUCTURAS."
         ?>
     </body>
 </html>
