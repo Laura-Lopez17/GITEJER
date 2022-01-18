af7c11b7ca76eb0a74d93ea46d9472907b304265
Laura-Lopez17 <al038381@ieslaencanta.com>
2022-01-18 19:16:09 +0100

    commit 4

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-    </head>
+        <title>Login</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>     
     <body>
+        <nav id="menu_nav">
+            <a href="#">INICIO</a>
+            <a href="#">Política de privacidad</a>
+            <a href="#">Login</a>
+            
+        </nav>
+        <div id="content">
+            <form id="login" action="ajax/form.php" method="post">
+                <label>Usuario:</label>
+                <input type="text" name="user">
+                <label>Password:</label>
+                <input type="text" name="pass">
+                <input id="submit_form" type="submit" value="Enviar">
+            </form>
+        </div>
         <?php
-        echo "AÑADIDAS LAS ESTRUCTURAS."
+            
         ?>
     </body>
 </html>
